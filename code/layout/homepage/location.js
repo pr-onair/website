3fecaa425ecc93da97df17c4cd917669fcb0befa
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-10-27 15:28:26 +1100

    added map
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

---
+++ code/layout/homepage/location.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+
+/**
+ * The Section component
+ */
+const Section = ({ headline, map, id, _body, _relativeURL, _ID }) => (
+	<section className={`section section-${ id }`}>
+		<h2 className="contentHeadline" id={ id } tabIndex="0">{ headline }</h2>
+		<div className="section-content">
+			{ _body }
+		</div>
+		<iframe className="row-h content location-map" src={ map } width="100%" height="100%" frameBorder="0" allowFullScreen></iframe>
+	</section>
+);
+
+Section.propTypes = {
+	/**
+	 * headline: Section
+	 */
+	headline: PropTypes.string.isRequired,
+
+	/**
+	 * id: uber-uns
+	 */
+	id: PropTypes.string,
+
+	/**
+	 * _body: (text)(12)
+	 */
+	_body: PropTypes.node.isRequired,
+};
+
+Section.defaultProps = {};
+
+export default Section;
--- content/index/kontakt.md
+++ content/index/kontakt.md
@@ -1,7 +1,8 @@
 ---
-layout: homepage/section
+layout: homepage/location
 id: kontakt
 headline: Kontakt
+map: https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2493.111806192965!2d12.373465951503768!3d51.32746333156403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a6f82d93c162c3%3A0x43c2a3975b2506fb!2sShakespearepl.+8%2C+04107+Leipzig%2C+Germany!5e0!3m2!1sen!2sau!4v1540614435998
 ---
 
 TODO
